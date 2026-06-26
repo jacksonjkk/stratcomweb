ffcdf3e8aa280176e3ad73e7ca4b4a784710ebe4
jacksonjkk <jackjunior301010@gmail.com>
2026-06-26 08:48:12 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
+import Carousel from './Carousel'
+import Aboutus from './Aboutus'
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <Carousel/>
+    <Aboutus/>
     </div>
   )
 }
